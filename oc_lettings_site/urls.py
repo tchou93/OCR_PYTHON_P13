e1bfa8370fb0905ab52c3540349f9d21a606d285
TRAN Tan <tan.tran.workpro@gmail.com>
2022-12-05 21:03:10 +0100

    [STEP2] New application Profiles has been created with all the old datas on the new table.

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -6,7 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('', include('apps.lettings.urls')),
-    path('profiles/', views.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', views.profile, name='profile'),
+    path('', include('apps.profiles.urls')),
     path('admin/', admin.site.urls),
 ]
--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from .models import Profile
 
 
 # Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie quam lobortis leo
@@ -10,21 +9,3 @@ from .models import Profile
 # cursus in. Praesent volutpat porttitor magna, non finibus neque cursus id.
 def index(request):
     return render(request, 'index.html')
-
-# Sed placerat quam in pulvinar commodo. Nullam laoreet consectetur ex, sed consequat libero
-# pulvinar eget. Fusc faucibus, urna quis auctor pharetra, massa dolor cursus neque, quis dictum
-# lacus d
-def profiles_index(request):
-    profiles_list = Profile.objects.all()
-    context = {'profiles_list': profiles_list}
-    return render(request, 'profiles_index.html', context)
-
-
-# Aliquam sed metus eget nisi tincidunt ornare accumsan eget lac laoreet neque quis,
-# pellentesque dui. Nullam facilisis pharetra vulputate. Sed tincidunt, dolor id facilisis
-# fringilla, eros leo tristique lacus, it. Nam aliquam dignissim congue. Pellentesque habitant
-# morbi tristique senectus et netus et males
-def profile(request, username):
-    profile_obj = Profile.objects.get(user__username=username)
-    context = {'profile': profile_obj}
-    return render(request, 'profile.html', context)
