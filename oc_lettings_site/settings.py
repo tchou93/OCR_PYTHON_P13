f5e311dc435ead95f0a417feaf9e881053a21802
TRAN Tan <tan.tran.workpro@gmail.com>
2022-12-07 21:44:26 +0100

    [FIX][STEP3] See the behaviour of the pipeline without reference of Heroku.

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,5 +1,5 @@
 import os
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -112,4 +112,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
