0d50795cf5f3fdfe34034bc99e6ab8020eeeefaf
TRAN Tan <tan.tran.workpro@gmail.com>
2022-12-09 00:58:05 +0100

    [STEP4] Add the website supervision with Sentry and add some global var to protect some information on the file settings.py.

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,4 +1,19 @@
 import os
+import environ
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+env = environ.Env()
+environ.Env.read_env()
+
+sentry_sdk.init(
+    dsn=env('SENTRY_DNS'),
+    integrations=[
+        DjangoIntegration(),
+    ],
+    traces_sample_rate=1.0,
+    send_default_pii=True
+)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -8,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -3,9 +3,13 @@ from django.urls import path, include
 
 app_name = 'main'
 
+def trigger_error(request):
+    return 1 / 0
+
 urlpatterns = [
     path('', include('apps.lettings.urls')),
     path('', include('apps.profiles.urls')),
     path('', include('apps.main.urls')),
     path('admin/', admin.site.urls),
+    path('sentry-debug/', trigger_error),
 ]
