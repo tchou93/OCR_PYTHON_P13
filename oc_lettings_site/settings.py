02d61fac37771ed938b663b6e3599fd5e0f0ba47
TRAN Tan <tan.tran.workpro@gmail.com>
2022-12-21 23:18:51 +0100

    Correct CSS BUG on admin page.

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -130,5 +130,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
