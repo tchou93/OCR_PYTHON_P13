ff1ca9911e0c770e8508ed50ea28d1e5a59e266c
TRAN Tan <tan.tran.workpro@gmail.com>
2022-12-22 23:39:26 +0100

    Correct warning during the workflow.

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -124,10 +124,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
