5bc5d88761f30732b5ff74d93ec3c7a9ec316e18
TRAN Tan <tan.tran.workpro@gmail.com>
2023-02-06 13:56:23 +0100

    [FIX] Correct the error in settings.py.

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'oc_lettings_site',
     'apps.lettings',
     'apps.profiles',
     'apps.main',
